9439def5ef3b91b7fb90eeaf7e48ddcc49acd86a
DevPGSV <DevPGSV@gmail.com>
2016-04-16 20:13:14 +0200

    Refactored database management plugin

--- src/TelegramBot/plugins/databaseManagementPlugin.php
+++ src/TelegramBot/plugins/databaseManagementPlugin.php
@@ -11,35 +11,51 @@ class DatabaseManagementPlugin extends TB_Plugin {
    * %condition date any
    */
   public function onMessageReceived($message) {
+    if ($this->getUserInDB($message->getFrom()->getId()) === null) {
+      $this->addUserToDB($message->getFrom());
+    }
+    $currentUser = $this->getUserInDB($message->getFrom()->getId());
+
+    $newData = [];
+    if (!in_array($message->getChat()->getId(), $currentUser['chatsSeen'])) {
+      $newData['$push']['chatsSeen'] = $message->getChat()->getId();
+    }
+    $newData['$inc']['stats.messages'] = 1;
+    if ($message->getFrom()->getFirstName() !== $currentUser['first_name']) $newData['$set']['first_name'] = $message->getFrom()->getFirstName();
+    if ($message->getFrom()->getLastName() !== $currentUser['last_name']) $newData['$set']['last_name'] = empty($message->getFrom()->getLastName())?'':$message->getFrom()->getLastName();
+    if ($message->getFrom()->getUsername() !== $currentUser['username']) $newData['$set']['username'] = empty($message->getFrom()->getUsername())?'':$message->getFrom()->getUsername();
+
+    echo '<pre>', print_r($newData, true), '</pre>';
+    $this->db->selectCollection('users')->updateOne(['_id' => $message->getFrom()->getId()], $newData);
+
+    //$message->sendReply('->```'.print_r($newData, true).'```', null, null, 'Markdown');
+  }
+
+  private function getUserInDB($userId) {
     $o = array(
       'typeMap' => array(
         'root' => 'array',
         'document' => 'array',
       ),
     );
-    $currentUser = $this->db->selectCollection('users', $o)->findOne(['_id' => $message->getFrom()->getId()]);
-    if ($currentUser === null) {
-      $currentUser = array(
-        "_id" => $message->getFrom()->getId(),
-        "first_name" => $message->getFrom()->getFirstName(),
-        "chatsSeen" => [$message->getChat()->getId()],
-        "role" => "user",
-        "ban" => array(
-          "status" => false,
-          "reason" => "none"
-        ),
-        "stats" => ['messages' => 0]
-      );
-      if ($message->getFrom()->getUsername() !== null) $currentUser['username'] = $message->getFrom()->getUsername();
-      if ($message->getFrom()->getLastName() !== null) $currentUser['last_name'] = $message->getFrom()->getLastName();
-      $this->db->selectCollection('users')->insertOne($currentUser);
-    }
-    if (!in_array($message->getChat()->getId(), $currentUser['chatsSeen'])) {
-      array_push($currentUser['chatsSeen'], $message->getChat()->getId());
-      $this->db->selectCollection('users')->updateOne(['_id' => $message->getFrom()->getId()], ['$set' => ['chatsSeen' => $currentUser['chatsSeen']]]);
-    }
-    $this->db->selectCollection('users')->updateOne(['_id' => $message->getFrom()->getId()], ['$inc' => ['stats.messages' => 1]]);
-    //$message->sendReply('->```'.print_r($currentUser, true).'```', null, null, 'Markdown');
+    return $this->db->selectCollection('users', $o)->findOne(['_id' => $userId]);
+  }
+
+  private function addUserToDB($user) {
+    $currentUser = array(
+      "_id" => $user->getId(),
+      "first_name" => $user->getFirstName(),
+      "username" => ($user->getUsername() !== null)?$user->getUsername():'',
+      "last_name" => ($user->getLastName() !== null)?$user->getLastName():'',
+      "chatsSeen" => [],
+      "role" => "user",
+      "ban" => array(
+        "status" => false,
+        "reason" => "none"
+      ),
+      "stats" => ['messages' => 0]
+    );
+    $this->db->selectCollection('users')->insertOne($currentUser);
   }
 
   public function getChangeLog() {
@@ -56,6 +72,12 @@ class DatabaseManagementPlugin extends TB_Plugin {
           'Added "getChangeLog" function',
         ],
       ],
+      '1460829949' => [
+        'version'=>[0, 2, 0, 'alpha'],
+        'changes' => [
+          'Refactored plugin',
+        ],
+      ],
     ];
   }
 }
@@ -63,5 +85,5 @@ return array(
   'class' => 'DatabaseManagementPlugin',
   'name' => 'Database Management',
   'id' => 'DatabaseManagementPlugin',
-  'version' => [0, 1, 0, 'alpha'],
+  'version' => [0, 2, 0, 'alpha'],
 );
